7d742d599a2fcab4c70f323ae6bb53be58ce914e
LkRonald <rpereirac@unicartagena.co>
2026-08-19 22:31:01 -0500

    declaracion de los record

---
+++ proyecto_c#/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+           
+
+        }
+    }
+
+
+    record Estudiante
+    {
+        public string Nombre { get; init; }
+        public int Edad { get; init; }
+        public double Promedio { get; init; }
+
+        public Estudiante(string nombre, int edad, double promedio)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Promedio = promedio;
+        }
+    }
+}
